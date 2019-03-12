2520a764623456abc3f8e43a21a4460e81931efa
Joice <joice@Joices-iMac.local>
2019-03-12 21:52:21 +0530

    class type dynamic checking

--- Playground/main.m
+++ Playground/main.m
@@ -26,9 +26,32 @@ void driver(){
     
 }
 
+void dynamicTyping(){
+    Car *delorean = [[Car alloc] initWithModel:@"DeLorean"];
+    // Get the class of an object
+    NSLog(@"%@ is an instance of the %@ class", [delorean model], [delorean class]);
+    // Check an object against a class and all subclasses
+    if ([delorean isKindOfClass:[NSObject class]]) {
+        NSLog(@"%@ is an instance of NSObject or one of its subclasses",
+                [delorean model]);
+        
+    } else {
+        NSLog(@"%@ is not an instance of NSObject or " "one of its subclasses",
+              [delorean model]);
+    }
+    // Check an object against a class, but not its subclasses
+    if ([delorean isMemberOfClass:[NSObject class]]) {
+        NSLog(@"%@ is a instance of NSObject",[delorean model]);
+        
+    } else {
+        NSLog(@"%@ is not an instance of NSObject", [delorean model]);
+    }
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         driver();
+        dynamicTyping();
     }
     return 0;
 }
