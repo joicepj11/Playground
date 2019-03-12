9cd6c642cfb6a0206399933e0fde1e7d0fef31f7
Joice <joice@Joices-iMac.local>
2019-03-12 15:43:52 +0530

    Initial Commit

---
+++ Playground/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Playground
+//
+//  Created by Joice on 12/03/19.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
