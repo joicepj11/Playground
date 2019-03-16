c606b493aa312448b3311564d7b5ef2b2f932070
Joice <joice@Joices-iMac.local>
2019-03-16 20:17:36 +0530

    NSNumber example

--- Playground/main.m
+++ Playground/main.m
@@ -49,11 +49,44 @@ void objectivecDataType(){
     }
 }
 
+void nsNumber(){
+    NSNumber *aBool = [NSNumber numberWithBool:NO];
+    NSNumber *aChar = [NSNumber numberWithChar:'z'];
+    NSNumber *aUChar = [NSNumber numberWithUnsignedChar:255];
+    NSNumber *aShort = [NSNumber numberWithShort:32767];
+    NSNumber *aUShort = [NSNumber numberWithUnsignedShort:65535];
+    NSNumber *anInt = [NSNumber numberWithInt:2147483647];
+    NSNumber *aUInt = [NSNumber numberWithUnsignedInt:4294967295];
+    NSNumber *aLong = [NSNumber numberWithLong:9223372036854775807];
+    NSNumber *aULong = [NSNumber numberWithUnsignedLong:1844674407370955169];
+    NSNumber *aFloat = [NSNumber numberWithFloat:26.99f];
+    NSNumber *aDouble = [NSNumber numberWithDouble:26.99];
+    NSLog(@"%@", [aBool boolValue] ? @"YES" : @"NO");
+    NSLog(@"%c", [aChar charValue]);
+    NSLog(@"%hhu", [aUChar unsignedCharValue]);
+    NSLog(@"%hi", [aShort shortValue]);
+    NSLog(@"%hu", [aUShort unsignedShortValue]);
+    NSLog(@"%i", [anInt intValue]);
+    NSLog(@"%u", [aUInt unsignedIntValue]);
+    NSLog(@"%li", [aLong longValue]);
+    NSLog(@"%lu", [aULong unsignedLongValue]);
+    NSLog(@"%f", [aFloat floatValue]);
+    NSLog(@"%f", [aDouble doubleValue]);
+    
+    float a = [aFloat floatValue];
+    NSLog(@"NSNumber to primtive data type %f",a);
+    
+    //Numeric Literals
+    NSNumber *aBoolLiteral = @NO;
+    
+    NSLog(@"Boolean literal %@",aBoolLiteral);
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        cdataType();
-        objectivecDataType();
-        
+//        cdataType();
+//        objectivecDataType();
+        nsNumber();
     }
     return 0;
 }
