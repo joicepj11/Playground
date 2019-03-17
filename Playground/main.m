556b3987da52c012b3e6a2f0e33a0fac43eb2733
Joice <joice@Joices-iMac.local>
2019-03-17 22:56:33 +0530

    ns dictionary example

--- Playground/main.m
+++ Playground/main.m
@@ -209,6 +209,35 @@ void nsArray(){
     NSLog(@"%@", sortedMakes);
 }
 
+void nsDictionary(){
+    // Literal syntax
+    NSDictionary *inventory =
+  @{
+    @"Mercedes-Benz SLK250" : [NSNumber numberWithInt:13],
+    @"Mercedes-Benz E350" : [NSNumber numberWithInt:22],
+    @"BMW M3 Coupe" : [NSNumber numberWithInt:19],
+    @"BMW X6" : [NSNumber numberWithInt:16],
+    };
+ 
+    // Values and keys as arguments
+    inventory = [NSDictionary dictionaryWithObjectsAndKeys:
+                 [NSNumber numberWithInt:13], @"Mercedes-Benz SLK250",
+                 [NSNumber numberWithInt:22], @"Mercedes-Benz E350",
+                 [NSNumber numberWithInt:19], @"BMW M3 Coupe",
+                 [NSNumber numberWithInt:16], @"BMW X6", nil];
+   
+    // Values and keys as arrays
+    NSArray *models = @[@"Mercedes-Benz SLK250", @"Mercedes-Benz E350", @"BMW M3 Coupe",@"BMW X6"];
+    
+    NSArray *stock = @[[NSNumber numberWithInt:13], [NSNumber numberWithInt:22], [NSNumber numberWithInt:19],[NSNumber numberWithInt:16]];
+    
+    inventory = [NSDictionary dictionaryWithObjects:stock forKeys:models];
+    NSLog(@"%@", inventory);
+    
+    NSLog(@"Models: %@", [inventory allKeys]);
+    NSLog(@"Stock: %@", [inventory allValues]);
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
 //        cdataType();
@@ -216,7 +245,8 @@ int main(int argc, const char * argv[]) {
 //        nsNumber();
 //        nsString();
         //nsSet();
-        nsArray();
+       // nsArray();
+        nsDictionary();
     }
     return 0;
 }
